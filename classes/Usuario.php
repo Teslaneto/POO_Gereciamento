2a11e53df2b812b427aa4afc2c90eb048580bb0b
Geraldo <onixblackcx@gmail.com>
2024-11-27 15:28:58 -0300

    Tirei_uma_função

--- classes/Usuario.php
+++ classes/Usuario.php
@@ -6,15 +6,6 @@ class Usuario {
         $this->db = $db;
     }
 
-    public function criarUsuario($nome, $email, $senha) {
-        $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)";
-        $this->db->query($sql, [
-            ':nome' => $nome,
-            ':email' => $email,
-            ':senha' => password_hash($senha, PASSWORD_DEFAULT)
-        ]);
-    }
-
     public function listarUsuarios() {
         return $this->db->query("SELECT * FROM tb_alunos")->fetchAll(PDO::FETCH_ASSOC);
     }
