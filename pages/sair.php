578d539e76fbf67f0410ed2fb0f6f406234282b8
Geraldo <onixblackcx@gmail.com>
2024-12-03 16:23:53 -0300

    refactor/no arquivo sair

--- pages/sair.php
+++ pages/sair.php
@@ -1,5 +1,6 @@
-<form action="../controllers/sairController.php" method="post">
-    <span>Voce deseja Sair</span>
-    <button name="sim">SIM</button>
-    <button name="nao">NÃO</button>
+<form class="text-center" action="../controllers/sairController.php" method="post">
+    <span class="spac">Voce deseja Sair</span>
+    <br>
+    <button class="btn btn-danger btn-sm mr-2" name="sim">SIM</button>
+    <button class="btn btn-primary btn-sm mr-2" name="nao">NÃO</button>
 </form>
