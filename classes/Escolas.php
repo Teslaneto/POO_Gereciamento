2d5b3ce24753cb819431091b21201d6de13eb94d
Geraldo <onixblackcx@gmail.com>
2024-12-03 16:09:21 -0300

    atualização top demais

--- classes/Escolas.php
+++ classes/Escolas.php
@@ -16,11 +16,25 @@ class Escolas
         return $listarEscolas;
     }
 
-    public function apagarEscola($id){
+    public function apagarEscola($id)
+    {
         $query = "DELETE FROM tb_escola WHERE id = :id";
         $this->db->query($query,
         [
             ":id" => $id
+        ]
+        );
+    }
+
+    public function adicionarEscolas($nome, $inep, $endereco)
+    {
+        $query = "INSERT INTO tb_escola FROM (nome, inep, endereco) 
+                  VALUES (:nome, :inep, :endereco)";
+        $this->db->query($query,
+        [
+            ":nome" => $nome,
+            ":inep" => $inep,
+            ":endereco" => $endereco
         ]
         );
     }
